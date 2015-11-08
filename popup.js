824d46f33e3b43824ef443a66af0b948fbdcd7e2
Vincent Duong <vincentduong@Vincents-MacBook-Pro.local>
2015-11-08 02:40:49 -0500

    Edited popup.js

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	var input = document.getElementById('input');
 	var start = document.getElementById('start');
 	var isInputVisible = false;
+	var keywords; //the array of keywords
 
 	// initially hide the input field
 	input.style.visibility = 'hidden';
@@ -31,114 +32,85 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 	// Make the actual CORS request.
-function makeCorsRequest() {
-  // All HTML5 Rocks properties support CORS.
-  // var url = 'http://cors.io/?u=http://en.wikipedia.org/wiki/Volcano';
-  var url = 'http://cors.io/?u=http://semantic-link.com/related.php?word=' + input.value;
+	function makeCorsRequest() {
+		// All HTML5 Rocks properties support CORS.
+		// var url = 'http://cors.io/?u=http://en.wikipedia.org/wiki/Volcano';
+		var url = 'http://cors.io/?u=http://semantic-link.com/related.php?word=' + input.value;
 
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
+		var xhr = createCORSRequest('GET', url);
+		if (!xhr) {
+			alert('CORS not supported');
+			return;
+		}
 
-  // Response handlers.
-  // was onload
+		// Response handlers.
+		// was onload
 
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    // var title = getTitle(text);
-    // alert('Response from CORS request to ' + url + ': ' + title);
-    // console.log('success' + text);
+		xhr.onload = function() {
+		var text = xhr.responseText;
+		// var title = getTitle(text);
+		// alert('Response from CORS request to ' + url + ': ' + title);
+		// console.log('success' + text);
 
-    var obj = JSON.parse(text);
-    if (obj.length != 0) {
-    	console.log(obj.length);
-    	console.log(obj[1].v);
-    	console.log(obj[2].v);
-    }
-    else {
-    	obj = input.value;
-    	console.log(obj);
-    }
-  };
+		keywords  = JSON.parse(text);
+		if (keywords.length >= 5) {
+			console.log(keywords.length);
+			console.log(keywords[1].v);
+			console.log(keywords[2].v);
+		}
+		else {
+			keywords = new Array(1);
+			keywords[0] = input.value;
+			console.log(keywords[0]);
+			console.log(keywords.length);
+		}
+		};
 
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
+		xhr.onerror = function() {
+			alert('Woops, there was an error making the request.');
+		};
 
-  xhr.send();
-}
+		xhr.send();
+	}
+
+	function makeCorsRequest2(url) {
+		var xhr = createCORSRequest('GET', url);
+		if (!xhr) {
+			alert('CORS not supported');
+			return;
+		}
+		xhr.onload = function() {
+		var text = xhr.responseText;
+		console.log("TEST " + text);
+		//SOMETHING SOMETHING SOMETHING
+		};
+
+		xhr.onerror = function() {
+			alert('Woops, there was an error making the request.');
+		};
+
+		xhr.send();
+	} 
 
 	// listener when the start button is selected
 	start.addEventListener('click', function() {
-		if (isInputVisible == false) {
+		chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+			if (isInputVisible == false) {
+				// shows input
+				input.style.visibility = 'visible';
+				input.focus();
+				input.placeholder = "Enter topic here...";
 
-			// shows input
-			input.style.visibility = 'visible';
-			input.focus();
-			input.placeholder = "Enter topic here...";
-
-			// sets the input to be visible
-			isInputVisible = true;
-		}
-		else {
-		    // var url = 'http://semantic-link.com/#/' + input.value;
-		    // console.log(input.value);
-		    // $.ajax({
-		    // 	type: "GET",
-		    // 	url: url,
-		    // 	async: false,
-		    // 	cache: false,
-		    // }).done(function(response){
-		    // 	console.log(response);
-		    // });
-			makeCorsRequest();
-
-            // var req = new XMLHttpRequest();
-            // req.open('GET', url, false);
-            // req.send(null);
-            // if(req.status == 200) {
-            //     dump(req.responseText);
-            //     console.log(req);
-            // }
-
-   //          var ua = navigator.userAgent.toLowerCase();
-			// if (!window.ActiveXObject)
-  	// 			req = new XMLHttpRequest();
-			// else if (ua.indexOf('msie 5') == -1)
-  	// 			req = new ActiveXObject("Msxml2.XMLHTTP");
-			// else
-  	// 			req = new ActiveXObject("Microsoft.XMLHTTP");
-
-		}
-		// chrome.tabs.getSelected(null, function(tab) {
-		// 	d = document;
-		// 	if (isInputVisible == false) {
-
-		// 		// shows input
-		// 		input.style.visibility = 'visible';
-		// 		input.focus();
-		// 		input.placeholder = "Enter topic here...";
-
-		// 		// sets the input to be visible
-		// 		isInputVisible = true;
-		// 	}
-		// 	else {
-		// 	    var url = 'http://semantic-link.com/#/' + input.value;
-		// 		var f = d.createElement('form');
-		// 		f.action = url;
-		// 		f.method = 'post';
-		// 		var i = d.createElement('input');
-		// 		i.type = 'hidden';
-		// 		i.name = 'url';
-		// 		i.value = tab.url;
-		// 		f.appendChild(i);
-		// 		d.body.appendChild(f);
-		// 		f.submit();
-
-
-		// 	}
-		// });
+				// sets the input to be visible
+				isInputVisible = true;
+			}
+			else {
+				var url_2 = tabs[0].url;
+				var tmp = url_2.replace("http://", "");
+				var tmp2 = "http://www.textise.net/showText.aspx?strURL=http%253A//" + tmp;
+				makeCorsRequest();
+				makeCorsRequest2(tmp2);
+			}
+		});
 	}, false);
 }, false);
